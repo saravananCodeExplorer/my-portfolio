3801335ae13eca56261a7155247e333dd758f17a
saravananCodeExplorer <evergoldhariny2007@gmail.com>
2024-11-23 10:47:28 +0530

    para added

--- src/Componets/Navbar/Navbar.tsx
+++ src/Componets/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ const Navbar = () => {
   return (
     <div>Navbar
         <h2>hai</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum magnam saepe ut sunt ullam adipisci magni molestias maxime veniam aspernatur omnis facilis autem itaque pariatur repudiandae voluptate, laboriosam dignissimos ipsum.</p>
         
     </div>
   )
