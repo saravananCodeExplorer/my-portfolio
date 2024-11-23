bef79f16bb3b1bf1eeb8b60b3ac8b504d6ed0b17
saravananCodeExplorer <evergoldhariny2007@gmail.com>
2024-11-23 11:13:01 +0530

    h3 added

--- src/Componets/Navbar/Navbar.tsx
+++ src/Componets/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>Navbar
         <h2>hai</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum magnam saepe ut sunt ullam adipisci magni molestias maxime veniam aspernatur omnis facilis autem itaque pariatur repudiandae voluptate, laboriosam dignissimos ipsum.</p>
-        
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium consectetur dolorem dolor libero tenetur, hic alias. Nam nihil alias porro sint, sed repellat molestias at, esse consequuntur iusto maiores accusantium!</h3>
     </div>
   )
 }
